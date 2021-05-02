a38fd0dbb78d0e402f265c2343b733c0981d7bd2
JunJun <jqkrwns@gmail.com>
2021-05-02 23:09:27 +0900

    프로젝트 설정

---
+++ .github/ISSUE_TEMPLATE/feature-template.md
@@ -0,0 +1,11 @@
+---
+name: Feature Template
+about: issues feature template
+title: "[API] Feature: "
+labels: Feature
+assignees: inpernal
+
+---
+
+### TODO
+-
---
+++ src/main/java/com/muyaho/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.muyaho.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
